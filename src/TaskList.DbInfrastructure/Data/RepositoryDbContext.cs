9959c7538bd2a7045e5a8c257d1c46b82c2f6c6c
korzanov <kda.krooly@gmail.com>
2023-02-20 06:30:31 +0600

    feat: common spec & refactor

--- src/TaskList.DbInfrastructure/Data/RepositoryDbContext.cs
+++ src/TaskList.DbInfrastructure/Data/RepositoryDbContext.cs
@@ -9,12 +9,9 @@ public class RepositoryDbContext : DbContext
     //public RepositoryDbContext(DbContextOptions options) : base(options) { }
     public RepositoryDbContext(DbContextOptions<RepositoryDbContext> options) : base(options) { }
 
-    public DbSet<TaskList.Domain.Entities.User> Users { get; set; }
     public DbSet<TaskList.Domain.Entities.Task> Tasks { get; set; }
     public DbSet<TaskList.Domain.Entities.TaskList> TaskLists { get; set; }
     public DbSet<TaskList.Domain.Entities.TaskComment> TaskComments { get; set; }
     public DbSet<TaskList.Domain.Entities.TaskStatus> TaskStatuses { get; set; }
     public DbSet<TaskList.Domain.Entities.TaskStatusRecord> TaskStatusRecords { get; set; }
-    
-    
 }
