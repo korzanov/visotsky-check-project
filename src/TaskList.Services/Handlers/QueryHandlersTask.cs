6b4a949c9b125ff3fda8c7a31bb765c5c660d584
korzanov <kda.krooly@gmail.com>
2023-02-21 13:12:15 +0600

    feat: get task

---
+++ src/TaskList.Services/Handlers/QueryHandlersTask.cs
@@ -0,0 +1,29 @@
+using Ardalis.GuardClauses;
+using AutoMapper;
+using MediatR;
+using TaskList.Contracts.Queries;
+using TaskList.Contracts.Responses;
+using TaskList.Domain.Repositories;
+using Task = TaskList.Domain.Entities.Task;
+
+namespace TaskList.Services.Handlers;
+
+public class QueryHandlersTask :
+    IRequestHandler<QueryTaskGet, ResponseTask>
+{
+    private readonly IRepositoryReadOnly<Domain.Entities.Task> _repository;
+    private readonly IMapper _mapper;
+
+    public QueryHandlersTask(IRepositoryReadOnly<Task> repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<ResponseTask> Handle(QueryTaskGet request, CancellationToken cancellationToken)
+    {
+        var existTask = await _repository.GetByIdAsync(request.Id, cancellationToken);
+        Guard.Against.NotFound(request.Id, existTask, nameof(existTask.Id));
+        return _mapper.Map<ResponseTask>(existTask);
+    }
+}
