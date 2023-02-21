3e1550de005b7ee497f3c019b724df6cfd214d29
korzanov <kda.krooly@gmail.com>
2023-02-21 13:23:07 +0600

    feat: get list task by task list

--- src/TaskList.Services/Handlers/QueryHandlersTask.cs
+++ src/TaskList.Services/Handlers/QueryHandlersTask.cs
@@ -4,21 +4,27 @@ using MediatR;
 using TaskList.Contracts.Queries;
 using TaskList.Contracts.Responses;
 using TaskList.Domain.Repositories;
-using Task = TaskList.Domain.Entities.Task;
+using TaskList.Domain.Specifications;
 
 namespace TaskList.Services.Handlers;
 
 public class QueryHandlersTask :
     IRequestHandler<QueryTaskGet, ResponseTask>,
-    IRequestHandler<QueryTaskGetAll, IEnumerable<ResponseTask>>
+    IRequestHandler<QueryTaskGetAll, IEnumerable<ResponseTask>>,
+    IRequestHandler<QueryTaskGetAllByTaskList, IEnumerable<ResponseTask>>
 {
     private readonly IRepositoryReadOnly<Domain.Entities.Task> _repository;
+    private readonly IRepositoryReadOnly<Domain.Entities.TaskList> _repositoryTaskList;
     private readonly IMapper _mapper;
 
-    public QueryHandlersTask(IRepositoryReadOnly<Task> repository, IMapper mapper)
+    public QueryHandlersTask(
+        IRepositoryReadOnly<Domain.Entities.Task> repository, 
+        IRepositoryReadOnly<Domain.Entities.TaskList> repositoryTaskList, 
+        IMapper mapper)
     {
         _repository = repository;
         _mapper = mapper;
+        _repositoryTaskList = repositoryTaskList;
     }
 
     public async Task<ResponseTask> Handle(QueryTaskGet request, CancellationToken cancellationToken)
@@ -32,5 +38,14 @@ public class QueryHandlersTask :
     {
         var allTasks = await _repository.ListAsync(cancellationToken);
         return allTasks.Select(task => _mapper.Map<ResponseTask>(task));
+    }
+
+    public async Task<IEnumerable<ResponseTask>> Handle(QueryTaskGetAllByTaskList request, CancellationToken cancellationToken)
+    {
+        var existTaskList = await _repositoryTaskList.GetByIdAsync(request.TaskListId, cancellationToken);
+        Guard.Against.NotFound(request.TaskListId, existTaskList, nameof(existTaskList.Id));
+        var spec = new SpecCommon<Domain.Entities.Task>(task => task.TaskListId == request.TaskListId);
+        var taskListTasks = await _repository.ListAsync(spec, cancellationToken);
+        return taskListTasks.Select(task => _mapper.Map<ResponseTask>(task));
     }
 }
