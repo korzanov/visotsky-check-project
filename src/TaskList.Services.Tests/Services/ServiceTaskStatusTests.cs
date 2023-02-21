ad2467f3655e93d4e16f190c5ee09f36bc14a119
korzanov <kda.krooly@gmail.com>
2023-02-21 14:13:12 +0600

    test: add test cases for task statuses

---
+++ src/TaskList.Services.Tests/Services/ServiceTaskStatusTests.cs
@@ -0,0 +1,50 @@
+using MediatR;
+using TaskList.Contracts.Commands;
+using TaskList.Contracts.Queries;
+using TaskList.Contracts.Responses;
+
+namespace TaskList.Services.Tests.Services;
+
+public class ServiceTaskStatusTests : IClassFixture<ServicesFixture>
+{
+    private readonly IMediator _mediator;
+
+    public ServiceTaskStatusTests(ServicesFixture servicesFixture)
+    {
+        _mediator = servicesFixture.Mediator;
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(5)]
+    public async void CmdSetDefaults_Success(int count)
+    {
+        for (var i = 0; i < count; i++)
+            await _mediator.Send(new CommandTaskStatusSetDefaults());
+
+        var statuses = (await _mediator.Send(new QueryTaskStatusGetAll())).ToArray();
+
+        Assert.Equal(ResponseTaskStatus.Defaults.Length, statuses.Length);
+        foreach(var status in ResponseTaskStatus.Defaults)
+            Assert.True(statuses.Contains(status));
+    }
+
+    [Fact]
+    public async void CmdGetDefault_Success()
+    {
+        var defaultStatus = await _mediator.Send(new QueryTaskStatusGetDefault());
+
+        Assert.Equal(ResponseTaskStatus.Default, defaultStatus);
+    }
+
+    [Fact]
+    public async void CmdGetAll_Success()
+    {
+        var statuses = (await _mediator.Send(new QueryTaskStatusGetAll())).ToArray();
+
+        Assert.True(ResponseTaskStatus.Defaults.Length <= statuses.Length);
+        foreach(var status in ResponseTaskStatus.Defaults)
+            Assert.True(statuses.Contains(status));
+    }
+}
